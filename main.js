4f3f383d39ddefdc7dfbf7230181af3bd5186664
Tesla_Ambassador <tesla-ambassador@users.noreply.github.com>
2021-05-18 12:00:31 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// hamburger menu open
+function openMenu() {
+    document.getElementById('pop-up-links').style.display = 'block';
+}
+
+// hamburger menu close
+function closeMenu() {
+    document.getElementById('pop-up-links').style.display = 'none';
+}
